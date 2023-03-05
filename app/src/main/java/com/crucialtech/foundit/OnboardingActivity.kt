997653340fce7948ac10fe8a89ad46e30ccfbc22
Crucialjun <nich.otieno@gmail.com>
2023-03-05 14:17:48 +0300

    sign in screen

--- app/src/main/java/com/crucialtech/foundit/OnboardingActivity.kt
+++ app/src/main/java/com/crucialtech/foundit/OnboardingActivity.kt
@@ -1,11 +1,24 @@
 package com.crucialtech.foundit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.crucialtech.foundit.databinding.ActivityOnboardingBinding
 
 class OnboardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnboardingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnGetStarted.setOnClickListener {
+            val intent  = Intent(this,MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+            finish()
+        }
     }
 }
