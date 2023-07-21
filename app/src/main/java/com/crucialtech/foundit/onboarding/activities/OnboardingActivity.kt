6420dcfe331538a46cddabb8402d1d175c103586
Nicholas Otieno <nich.otieno@gmail.com>
2023-07-21 21:59:43 +0300

    change theme

--- app/src/main/java/com/crucialtech/foundit/onboarding/activities/OnboardingActivity.kt
+++ app/src/main/java/com/crucialtech/foundit/onboarding/activities/OnboardingActivity.kt
@@ -1,8 +1,13 @@
 package com.crucialtech.foundit.onboarding.activities
 
 import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.crucialtech.foundit.R
 import com.crucialtech.foundit.auth.activities.AuthActivity
 import com.crucialtech.foundit.databinding.ActivityOnboardingBinding
 
@@ -11,8 +16,12 @@ class OnboardingActivity : AppCompatActivity() {
     private lateinit var binding: ActivityOnboardingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //window.statusBarColor = ContextCompat.getColor(this,R.color.white)
+        //window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            window.colorMode = ActivityInfo.COLOR_MODE_WIDE_COLOR_GAMUT
+        }
         binding = ActivityOnboardingBinding.inflate(layoutInflater)
+
         val view = binding.root
         setContentView(view)
 
